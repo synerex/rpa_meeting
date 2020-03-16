a556b9a5912e2766e9dd8089bae6c8dd995822ba
hktm <mtknho@gmail.com>
2020-03-16 16:04:08 +0900

    add a test sample of desknets

--- meeting-provider.go
+++ meeting-provider.go
@@ -16,6 +16,7 @@ import (
 	"github.com/tidwall/gjson"
 
 	cybozu "github.com/synerex/meeting_cybozu"
+	desknets "github.com/synerex/meeting_desknets"
 
 	sxutil "github.com/synerex/synerex_sxutil"
 )
@@ -121,6 +122,12 @@ func setMeetingService(json string) {
 
 func demandCallback(clt *sxutil.SXServiceClient, dm *api.Demand) {
 	log.Println("Got Meeting demand callback")
+
+	facilities, err := desknets.Schedule(rm.Year, rm.Month, rm.Day, rm.Week, rm.Start, rm.End, rm.Title, rm.Room)
+	if err := nil {
+		log.Fatalln(err)
+	}
+	log.Println(facilities)
 
 	if dm.TargetId != 0 { // selected
 
